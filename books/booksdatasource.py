8820644ec45d727083f4171776b2d488176d2750
Skyler Kessenich <skylerkessenich@Skylers-MBP.lan1>
2021-10-02 20:54:40 -0500

    booksdatasource

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -9,6 +9,7 @@
 
 import csv
 
+
 class Author:
     def __init__(self, surname='', given_name='', birth_year=None, death_year=None):
         self.surname = surname
@@ -36,19 +37,43 @@ class Book:
 
 class BooksDataSource:
     def __init__(self, books_csv_file_name):
-        ''' The books CSV file format looks like this:
-
-                title,publication_year,author_description
-
-            For example:
-
-                All Clear,2010,Connie Willis (1945-)
-                "Right Ho, Jeeves",1934,Pelham Grenville Wodehouse (1881-1975)
-
+        ''' 
             This __init__ method parses the specified CSV file and creates
             suitable instance variables for the BooksDataSource object containing
             a collection of Author objects and a collection of Book objects.
         '''
+        file = open(books_csv_file_name)
+        csvreader=csv.reader(file)
+        header=next(csvreader)
+        self.booksList = []
+        self.authorsList=[]
+        for book in csvreader:
+            #read every book and parse it into the book and author objects after this function, we should have a list of books and authors
+            counter=0
+            author=book[2].split('and')
+            authorsLocal=[]
+            while len(author)>counter: #extracts info about the author
+                authorI=author[counter].split('(')
+                authorName=authorI[0].strip()
+                authorName=authorName.split(' ',1)
+                givenName=authorName[0]
+                surname=authorName[1]
+                lifeSpan=authorI[1].split('-')
+                birthYear=int(lifeSpan[0])
+                deathYear=lifeSpan[1].strip(')').strip()
+                if deathYear=='':
+                    deathYear==None
+                else:
+                    deathYear=int(deathYear)
+                newAuthor=Author(surname,givenName,birthYear,deathYear)
+                if newAuthor not in self.authorsList: #adds newAuthor to list of all authors
+                    self.authorsList.append(newAuthor)
+                authorsLocal.append(newAuthor) #adds newAuthor to list of authors for this book
+                counter+=1
+            newBook=Book(book[0],int(book[1]),authorsLocal)
+            self.booksList.append(newBook)
+
+       
         pass
 
     def authors(self, search_text=None):
@@ -57,7 +82,16 @@ class BooksDataSource:
             returns all of the Author objects. In either case, the returned list is sorted
             by surname, breaking ties using given name (e.g. Ann Brontë comes before Charlotte Brontë).
         '''
-        return []
+        authorsReturned=[]
+        if search_text==None:
+            return self.authorsList
+        search_text=search_text.lower()
+        for author in self.authorsList:
+            fullName=author.given_name+author.surname
+            if search_text in fullName.lower():
+                authorsReturned.append(author)
+        authorsReturned.sort(key=lambda x:(x.surname,x.given_name),reverse=False)
+        return authorsReturned
 
     def books(self, search_text=None, sort_by='title'):
         ''' Returns a list of all the Book objects in this data source whose
@@ -71,7 +105,20 @@ class BooksDataSource:
                 default -- same as 'title' (that is, if sort_by is anything other than 'year'
                             or 'title', just do the same thing you would do for 'title')
         '''
-        return []
+
+        booksReturned=[]
+        if search_text==None:
+            booksReturned=self.booksList
+        else:
+            search_text=search_text.lower()
+            for book in self.booksList:
+                if search_text in book.title.lower():
+                    booksReturned.append(book)
+        if sort_by=='year':
+            booksReturned.sort(key=lambda x:x.publication_year,reverse=False)
+        else:
+            booksReturned.sort(key=lambda x:x.title,reverse=False)
+        return booksReturned
 
     def books_between_years(self, start_year=None, end_year=None):
         ''' Returns a list of all the Book objects in this data source whose publication
@@ -84,5 +131,36 @@ class BooksDataSource:
             during start_year should be included. If both are None, then all books
             should be included.
         '''
-        return []
+        booksReturned=[]
+        if start_year==None and end_year==None:
+            booksReturned=self.booksList
+        elif start_year==None:
+            for book in self.booksList:
+                if book.publication_year<=end_year:
+                    booksReturned.append(book)
+        elif end_year==None:
+            for book in self.booksList:
+                if book.publication_year>=start_year:
+                    booksReturned.append(book)
+        else:
+            for book in self.booksList:
+                if book.publication_year>=start_year and book.publication_year<=end_year:
+                    booksReturned.append(book)
+
+        booksReturned.sort(key=lambda x:x.publication_year, reverse=False)
+        return booksReturned
+
+
+def main():
+    tester=BooksDataSource('books1.csv')
+    test1=tester.authors('Brontë')
+    for a in test1:
+        print(a.given_name)
+
+    
+
+
+
+if __name__ == '__main__':
+    main()
 
