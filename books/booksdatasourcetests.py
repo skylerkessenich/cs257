9ac924155f8faa8dcfcccf34363870e9aa432b0c
Skyler Kessenich <skylerkessenich@Skylers-MBP.lan1>
2021-10-02 20:53:27 -0500

    updated tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -16,7 +16,7 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_unique_author(self):
         authors = self.data_source.authors('Pratchett')
         self.assertTrue(len(authors) == 1)
-        self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
+        self.assertTrue(authors[0].birth_year == 1948)
     def testAuthorBasics(self):
       #tests the basics of the author class
         authors= self.data_source.authors('Pratchett')
@@ -26,7 +26,7 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_unique_book(self):
         book = self.data_source.books('Good Omens')
         self.assertTrue(len(book) == 1)
-        self.assertTrue(book[0] == Book('Good Omens'))
+        self.assertTrue(book[0].title == 'Good Omens')
     def testBookBasics(self):
       #tests the bascis of the book class
         book= self.data_source.books("Sense and Sensibility")
@@ -34,21 +34,21 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(book[0].title=="Sense and Sensibility")
         self.assertTrue(book[0].publication_year==1813)
         print('here')
-        self.assertTrue(book[0].author[0]==Author ('Austen','Jane'))
+        self.assertTrue(book[0].authors[0].death_year==1817)
     def testDifferentAuthorSameName(self):
         #Checks to see if both authors with the same name are returned by author
-        authors= self.data_source.authors('bronte')
-        self.assertTrue(len(authors)==2)
-        self.assertTrue(authors[0].given_name=='Charlotte')
-        self.assertTrue(authors[1].given_name=='Emily')
+        authors= self.data_source.authors('brontë')
+        self.assertTrue(len(authors)==3)
+        self.assertTrue(authors[0].given_name=='Ann')
+        self.assertTrue(authors[2].given_name=='Emily')
     def testTwoAuthorsOneBook(self):
         #checks to see if both athors are in Good Omens
         book = self.data_source.books('Good Omens')
-        self.assertTrue(book[0].author[0].surname=='Gaiman')
+        self.assertTrue(book[0].authors[0].surname=='Gaiman')
     def testGetAllBooks(self):
         #should return all the books
         book=self.data_source.books()
-        self.assertTrue(len(book)==41)
+        self.assertTrue(len(book)==40)
     def testgetAllER(self):
       #gets all books with an 'er'
         books=self.data_source.books('er')
@@ -63,7 +63,7 @@ class BooksDataSourceTester(unittest.TestCase):
       books=self.data_source.books_between_years(2018,2020)
       self.assertTrue(len(books)==4)
       self.assertTrue(books[0].title=="There, There")
-      self.assertTrue(books[0].title=="The Invisible Life of Addie LaRue")
+      self.assertTrue(books[3].title=="The Invisible Life of Addie LaRue")
   
 
     
