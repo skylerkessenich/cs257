109f50af91743a01e0562b512380f061db0033dc
Skyler Kessenich <skylerkessenich@Skylers-MacBook-Pro.local>
2021-10-06 11:12:37 -0500

    added books

--- books/books.py
+++ books/books.py
@@ -4,10 +4,13 @@ import booksdatasource
 import sys
 
 def books(args,bookList):
+	print('hlll')
 	if len(args)<=2:
 		return bookList.books()
 	elif args[2]=='-S':
-		if args[4]=='y'
+		if len(args)<5:
+			return bookList.books(args[3])
+		elif args[4]=='y':
 			return bookList.books(args[3],'years')
 		else:
 			return bookList.books(args[3])
@@ -49,42 +52,46 @@ def between(args,bookList):
 
 
 def usage(function=None):
-	f=open('usage.txt',r)
-	contents=f.read()
+	f=open('usage.txt','r')
+	contents=f.readlines()
 	if function==None:
 		print(contents)
 	elif function=='books':
-		lines=[8,20]
-		for i, line in enumerate(f):
+		lines=[8,9,10,11,12,13,14,15,16,17,18,19,20]
+		for i, line in enumerate(contents):
 			if(i in lines):
 				print(line)
 	elif function=='authors':
-		lines=[26,32]
-		for i, line in enumerate(f):
+		lines=[26,27,28,29,30,31,32]
+		for i, line in enumerate(contents):
 			if(i in lines):
 				print(line)
 	elif function=='between':
-		lines=[35,40]
-		for i, line in enumerate(f):
+		lines=[35,36,37,38,39,40]
+		for i, line in enumerate(contents):
 			if(i in lines):
 				print(line)
 	elif function=='usage':
-		lines=[47,54]
-		for i, line in enumerate(f):
+		lines=[47,48,49,50,51,52,53,54]
+		for i, line in enumerate(contents):
 			if(i in lines):
 				print(line)
 	else:
 		print(contents)
+	f.close()
+
 
 def main():
 	bookList=booksdatasource.BooksDataSource("books1.csv")
-	if sys.argv[1]=='-t' or '--titles':
+	print(sys.argv[1])
+	if sys.argv[1]=='-t' or sys.argv[1]=='--titles':
 		returnList=books(sys.argv,bookList)
-	elif sys.argv[1]=='-a' or '--authors':
+	elif sys.argv[1]=='-a' or sys.argv[1]=='--authors':
+		
 		returnList=authors(sys.argv,bookList)
-	elif sys.argv[1]=='-b' or '--between':
+	elif sys.argv[1]=='-b' or sys.argv[1]=='--between':
 		returnList=between(sys.argv,bookList)
-	elif sys.argv[1]=='-u' or '--usage':
+	elif sys.argv[1]=='-u' or sys.argv[1]=='--usage':
 		if sys.argv[2]=='-t' or '--titles':
 			usage('books')
 		elif sys.argv[2]=='-a' or '--authors':
@@ -95,8 +102,12 @@ def main():
 	else:
 		usage()
 		returnList=None
-	for i in returnList:
-		print(i)
+	if returnList !=None:
+		for i in returnList:
+			if type(i)==booksdatasource.Book:
+				print(i.title)
+			else:
+				print(i.given_name+' '+i.surname)
 
 
 
